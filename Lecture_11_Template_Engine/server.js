773edc795497b45e1d7318e5a1acb4e122b1301b
khorshedsadhin <khorshedsadhin81@gmail.com>
2025-04-10 22:06:18 +0600

    ejs

--- Lecture_11_Template_Engine/server.js
+++ Lecture_11_Template_Engine/server.js
@@ -2,6 +2,17 @@ import express from 'express';
 
 const app = express();
 
+let product = [
+    {title: 'Iphone - 16', price: 120000},
+    {title: 'Galaxy S25', price: 79000},
+    {title: 'Google Pixel', price: 55000},
+]
+
+app.get('/', (req, res) => {
+    let name = "Khorshed Sadhin";
+    res.render('index.ejs', {name, product});
+})
+
 const port = 3000;
 app.listen(port, (req, res) => {
     console.log(`server is listening on port ${port}`);
